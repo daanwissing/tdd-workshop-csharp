d741880147c28ccc246268a0c7d0036738368967
Daan Wissing <daan.wissing@gmail.nl>
2022-10-27 10:47:29 +0200

    Implement most of it

--- TennisKata.Tests/TennisGameTests.cs
+++ TennisKata.Tests/TennisGameTests.cs
@@ -13,11 +13,104 @@ namespace TennisKata.Tests
             game = new TennisGame();
         }
 
-        [Test]
-        public void WhenNoScore_PrintReturnsLoveAll()
+        [TestCaseSource(nameof(GiveTestCases))]
+        public void GivenScoreForPlayers_ExpectedScoreIsPrinted(int scorePlayer1, int scorePlayer2, string expectedScore)
         {
+            // Arrange
+            SetScore(scorePlayer1, scorePlayer2);
+
+            // Act
             var result = game.PrintScore();
-            result.Should().Be("Love All");
+
+            // Assert
+            result.Should().Be(expectedScore);
+        }
+
+        [Test]
+        public void GivenDeuce_WhenPlayer1Scores_AdvantagePlayer1()
+        {
+            // Arrange
+            SetScore(3, 3);
+            game.Player1Scores();
+
+            // Act
+            var result = game.PrintScore();
+
+            // Assert
+            result.Should().Be("Advantage Player 1");
+        }
+
+        [Test]
+        public void GivenDeuce_WhenPlayer1ScoresAndPlayer2Scores_DeuceAgain()
+        {
+            // Arrange
+            SetScore(3, 3);
+            game.Player1Scores();
+            game.Player2Scores();
+
+            // Act
+            var result = game.PrintScore();
+
+            // Assert
+            result.Should().Be("Deuce");
+        }
+
+        [Test]
+        public void GivenDeuce_WhenPlayer2ScoresAndPlayer1Scores_DeuceAgain()
+        {
+            // Arrange
+            SetScore(3, 3);
+            game.Player2Scores();
+            game.Player1Scores();
+
+            // Act
+            var result = game.PrintScore();
+
+            // Assert
+            result.Should().Be("Deuce");
+        }
+
+        private void SetScore(int scorePlayer1, int scorePlayer2)
+        {
+            for (int i = 0; i < scorePlayer1; i++)
+            {
+                game.Player1Scores();
+            }
+            for (int i = 0; i < scorePlayer2; i++)
+            {
+                game.Player2Scores();
+            }
+        }
+
+        private static object[] GiveTestCases()
+        {
+            return new object[]
+            {
+                new object[]{0,0,"Love All" },
+                new object[]{0,1,"Love Fifteen" },
+                new object[]{0,2,"Love Thirty" },
+                new object[]{0,3,"Love Forty" },
+                new object[]{1,0,"Fifteen Love" },
+                new object[]{1,1,"Fifteen All" },
+                new object[]{1,2,"Fifteen Thirty" },
+                new object[]{1,3,"Fifteen Forty" },
+                new object[]{2,0,"Thirty Love" },
+                new object[]{2,1,"Thirty Fifteen" },
+                new object[]{2,2,"Thirty All" },
+                new object[]{2,3,"Thirty Forty" },
+                new object[]{3,0,"Forty Love" },
+                new object[]{3,1,"Forty Fifteen" },
+                new object[]{3,2,"Forty Thirty" },
+                new object[]{3,3,"Deuce" },
+                new object[]{4,0,"Winner Player 1" },
+                new object[]{4,1,"Winner Player 1" },
+                new object[]{4,2,"Winner Player 1" },
+                new object[]{4,3,"Winner Player 1" }, // Player 1 was the first to get to 4 points with > 1 point difference. Player 2 scoring after that should be ignored
+                new object[]{0,4,"Winner Player 2" },
+                new object[]{1,4,"Winner Player 2" },
+                new object[]{2,4,"Winner Player 2" },
+                new object[]{3,4,"Advantage Player 2" },
+            };
         }
     }
 }
--- TennisKata/TennisGame.cs
+++ TennisKata/TennisGame.cs
@@ -2,19 +2,75 @@
 {
     public class TennisGame
     {
+        private readonly IReadOnlyDictionary<int, string> _scorePrints = new Dictionary<int, string>
+        {
+            {0, "Love" },
+            {1, "Fifteen" },
+            {2, "Thirty" },
+            {3, "Forty" }
+        };
+
+        private int score1;
+        private int score2;
+
+        private string? winner;
+
         public string PrintScore()
         {
-            throw new NotImplementedException();
+            if (score1 >= 3 && score2 == score1)
+            {
+                return "Deuce";
+            }
+
+            if (!string.IsNullOrEmpty(winner))
+            {
+                return $"Winner {winner}";
+            }
+
+            if (score1 >= 4)
+            {
+                return "Advantage Player 1";
+            }
+
+            if (score2 >= 4)
+            {
+                return "Advantage Player 2";
+            }
+
+            var player1Score = _scorePrints[score1];
+            string player2Score = score1 == score2 ? "All" : _scorePrints[score2];
+            return $"{player1Score} {player2Score}";
         }
 
         public void Player1Scores()
         {
-            throw new NotImplementedException();
+            score1++;
+            CheckWinner();
         }
 
         public void Player2Scores()
         {
-            throw new NotImplementedException();
+            score2++;
+            CheckWinner();
+        }
+
+        private void CheckWinner()
+        {
+            if (score1 >= 4)
+            {
+                if (score1 - score2 >= 2)
+                {
+                    winner = "Player 1";
+                }
+            }
+
+            if (score2 >= 4)
+            {
+                if (score2 - score1 >= 2)
+                {
+                    winner = "Player 2";
+                }
+            }
         }
     }
 }
